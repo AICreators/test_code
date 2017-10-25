bd0313c41225eef7c3a00e0a49eeeeb4b8aa38f8
Charles Yang <mryang@minelandcn.com>
2017-10-25 20:58:50 +0800

    放的蛤玩意儿啊。。删掉

--- xbee_receive.py
+++
@@ -1,8 +0,0 @@
-import time
-from hachi.serial import XBeeSerial
-x = XBeeSerial('/dev/ttyUSB0')
-while True:
-	#time.sleep(0)
-	print("<<<<<")
-	data = x.read_response()
-	print("%s celsius" % data.data.decode('utf-8'))
